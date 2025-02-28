1fc1a4e1a3914d72995cd188b8af1c3950c0c559
Supul Kalhara <63876795+supul-k@users.noreply.github.com>
2025-02-28 12:21:43 +0530

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -75,7 +75,7 @@ const Hero = () => {
                 Web solutions from front to back
             </motion.div>
             <div className="imageContainer">
-                <img src="/images/heroes.png" alt="hero" />
+                <img src="/images/1.png" alt="hero" />
             </div>
         </div>
     )
